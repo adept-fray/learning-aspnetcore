6c0fcd673253f6463a4be67026e55e91706602de
adept-fray <siavash.kh41@gmail.com>
2025-05-14 16:10:41 +0200

    Added Tasks Razor Page with GET/POST logic

---
+++ RazorTaskManager/Pages/Tasks.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorTaskManager.Pages
+{
+    public class TasksModel : PageModel
+    {
+        private static List<string> _taskList { get; set; } = new();
+
+        public List<string> TaskList => _taskList;
+
+        [BindProperty]
+        public string NewTask { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!string.IsNullOrEmpty(NewTask))
+                TaskList.Add(NewTask);
+
+            return RedirectToPage();
+        }
+    }
+}
