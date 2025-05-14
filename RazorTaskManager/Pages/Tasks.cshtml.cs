192f9cf251a6ec7c901b89126708c1cb364acae7
adept-fray <siavash.kh41@gmail.com>
2025-05-14 16:45:26 +0200

    Added TaskItem model and delete feature to Task Manager

--- RazorTaskManager/Pages/Tasks.cshtml.cs
+++ RazorTaskManager/Pages/Tasks.cshtml.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorTaskManager.Models;
 
 namespace RazorTaskManager.Pages
 {
     public class TasksModel : PageModel
     {
-        private static List<string> _taskList { get; set; } = new();
+        private static List<TaskItem> _taskList { get; set; } = new();
+        private static int _nextId = 1;
 
-        public List<string> TaskList => _taskList;
+        public List<TaskItem> TaskList => _taskList;
 
         [BindProperty]
         public string NewTask { get; set; }
@@ -19,7 +21,22 @@ namespace RazorTaskManager.Pages
         public IActionResult OnPost()
         {
             if (!string.IsNullOrEmpty(NewTask))
-                _taskList.Add(NewTask);
+            {
+                _taskList.Add(new TaskItem
+                {
+                    Id = _nextId++,
+                    Description = NewTask
+                });
+            }
+
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            var task = _taskList.FirstOrDefault(t => t.Id == id);
+            if (task != null)
+                _taskList.Remove(task);
 
             return RedirectToPage();
         }
